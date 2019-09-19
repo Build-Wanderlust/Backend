a13982c8c2b40c12cb1b50127e2d1b68e59e7b0d
Your Name <anthonyvigz@gmail.com>
2019-09-19 12:55:13 -0400

    set up migrations table and initializating users router

--- data/migrations/20190919122922_create-tables.js
+++ data/migrations/20190919122922_create-tables.js
@@ -1,8 +1,11 @@
-
 exports.up = function(knex) {
-  
-};
+    return (knex.schema.createTable('users', tbl => {
+        tbl.increments('id');
+        tbl.string('username', 128).notNullable().unique();
+    })
+)};
 
 exports.down = function(knex) {
-  
-};
+    return knex.schema
+      .dropTableIfExists('users')
+  };
