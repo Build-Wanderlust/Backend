55cc1f4f58f28cde787c7b9609eb9e036e82d3f6
Your Name <anthonyvigz@gmail.com>
2019-09-19 15:14:49 -0400

    added a few more function to users model

--- helpers/usersModel.js
+++ helpers/usersModel.js
@@ -1,9 +1,19 @@
 const db = require('../data/dbConfig.js');
 
 module.exports = {
-    find
+    find,
+    findBy,
+    findById
 }
 
 function find() {
     return db('users').select('id', 'username');
 }
+
+function findBy(filter) {
+    return db('users').where(filter);
+}
+
+function findById(id) {
+    return db('users').where({ id }).first();
+  }
