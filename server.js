02f38efcbc46dc383047daa95816c3232ee8d22a
Your Name <anthonyvigz@gmail.com>
2019-09-19 10:17:45 -0400

    finished installing dependencies and set up main server endpoint and index

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
-server = require('express');
+/// importing dependencies and middleware
 
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+
+const server = express();
+
+/// applying middleware 
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+/// main get endpoint
+
+server.get('/', (req, res) => {
+    res.send("Welcome to Wanderlust API")
+});
+
+module.exports = server;
