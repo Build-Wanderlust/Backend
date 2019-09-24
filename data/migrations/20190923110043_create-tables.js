51c13344b94b1fbe312ab564e83e190079c40992
Your Name <anthonyvigz@gmail.com>
2019-09-24 12:27:57 -0400

    added faker and created 50 fake users to start the seed

--- data/migrations/20190923110043_create-tables.js
+++ data/migrations/20190923110043_create-tables.js
@@ -1,11 +1,18 @@
 exports.up = function(knex) {
-    return (knex.schema.createTable('users', tbl => {
-        tbl.increments('id');
-        tbl.string('firstname', 128).notNullable();
-        tbl.string('lastname', 128).notNullable();
-        tbl.string('email', 128).notNullable().unique();
-        tbl.string('password', 128).notNullable();
-    })
+    return (knex.schema
+        .createTable('users', tbl => {
+            tbl.increments('id');
+            tbl.string('firstname', 128).notNullable();
+            tbl.string('lastname', 128).notNullable();
+            tbl.string('email', 128).notNullable().unique();
+            tbl.string('password', 128).notNullable();
+        })
+        .createTable('experiences', tbl => {
+            tbl.increments('id');
+            tbl.string('title', 128).notNullable();
+            tbl.string('summary', 700).notNullable();
+            
+        })
 )};
 
 exports.down = function(knex) {
