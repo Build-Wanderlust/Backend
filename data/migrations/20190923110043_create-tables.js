8db62814fe11e9289037d45b483f2fbaf87836e5
Your Name <anthonyvigz@gmail.com>
2019-09-30 12:54:41 -0400

    removed experience seed and created new table for experiences with requested data

--- data/migrations/20190923110043_create-tables.js
+++ data/migrations/20190923110043_create-tables.js
@@ -9,15 +9,20 @@ exports.up = function(knex) {
         })
         .createTable('experiences', tbl => {
             tbl.increments('id');
-            tbl.integer('user_id').references('id').inTable('users');
             tbl.string('title', 128).notNullable();
-            tbl.string('summary', 700).notNullable();
-            tbl.string('imgUrl', 255);
-
+            tbl.string('description', 700).notNullable();
+            tbl.string('backgroundImg', 255).notNullable();
+            tbl.string('reviews', 128).notNullable();
+            tbl.string('location', 128).notNullable();
+            tbl.string('share', 128);
+            tbl.integer('ratings', 10).notNullable();
+            tbl.integer('reviews', 10).notNullable();
+            tbl.string('faveIt', 10);
         })
 )};
 
 exports.down = function(knex) {
     return knex.schema
       .dropTableIfExists('users')
+      .dropTableIfExists('experiences')
   };
