c393f42d48a67508473295164033fedaa64bf3ee
Your Name <anthonyvigz@gmail.com>
2019-10-01 14:59:12 -0400

    successfully initiated get request test

--- knexfile.js
+++ knexfile.js
@@ -26,6 +26,13 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: './data/test.db3',
-    }
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
   }
 };
